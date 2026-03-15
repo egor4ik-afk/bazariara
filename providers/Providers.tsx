f463da264a000578264d1d9d25e5e0fa0df5644c
webbuildge <webbuildge@gmail.com>
2026-03-15 11:35:44 +0000

    UserProvider

--- providers/Providers.tsx
+++ providers/Providers.tsx
@@ -1,17 +1,14 @@
 'use client'
 import React from 'react'
-import { UserProvider } from './UserProvider'
-import { CartProvider } from './CartProvider'
+import { CartProvider } from '@/contexts/CartContext'
 import { OrderProvider } from '@/contexts/OrderContext'
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <UserProvider>
-      <CartProvider>
-        <OrderProvider>
-          {children}
-        </OrderProvider>
-      </CartProvider>
-    </UserProvider>
+    <CartProvider>
+      <OrderProvider>
+        {children}
+      </OrderProvider>
+    </CartProvider>
   )
 }
